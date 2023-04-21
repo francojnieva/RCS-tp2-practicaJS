e22d727f1b6b0b00632f103267e2167476ddc4c4
francojnieva <franco97nieva@hotmail.com>
2023-04-21 18:00:46 -0300

    :memo: add exercise

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,11 @@
 // 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+let edad = prompt("Por favor, ingrese su edad")
+
+edad = parseInt(edad)
+
+if (edad === Number(edad)) {
+    
+    edad >= 18 ? document.write("Puedes conducir") : document.write("No puedes conducir, eres menor de edad")
+
+} else document.write("Por favor ingrese un número")
